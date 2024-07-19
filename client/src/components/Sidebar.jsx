abcb4065b555c842d8dce441b57b010a7ae0f031
vinaypanwar123 <vinaypanwar123s@gmail.com>
2024-07-19 11:39:49 +0530

    Changes in Sidebar components and App.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -48,6 +48,12 @@ const Sidebar = () => {
           <p className="font-bold">Search</p>
         </div>
       </div>
+
+
+
+
+
+      
       <div className="bg-[#121212] h-[85%] rounded">
         <div className="p-4 flex items-center justify-between">
           <div className="flex items-center gap-3">
@@ -59,22 +65,6 @@ const Sidebar = () => {
             <img className="w-5" src={assets.plus_icon} alt="" />
           </div>
         </div>
-
-        {/* <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
-          <h1>Create your first playlist</h1>
-          <p className="font-light">it's easy we will help you</p>
-          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
-            Create Playlist
-          </button>
-        </div>
-
-        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
-          <h1>Let's findsome podcast</h1>
-          <p className="font-light">we'll keep update on new episode</p>
-          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
-            Browse Podcats 
-          </button>
-        </div> */}
 
 <div className="overflow-y-auto h-[calc(100%-4rem)]">
           {data.length > 0 ? (
